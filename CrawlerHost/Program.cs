789d9d0b36091d25cbf163d3f983572184ed6da2
kangze <kangze25@hotmail.com>
2019-07-19 17:36:42 +0800

    1.config serilog

--- CrawlerHost/Program.cs
+++ CrawlerHost/Program.cs
@@ -1,12 +1,45 @@
 ﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Events;
 
 namespace CrawlerHost
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var host = new HostBuilder()
+                .ConfigureAppConfiguration(config =>
+                {
+                    if (File.Exists("appsettings.json"))
+                    {
+                        config.AddJsonFile("appsettings.json");
+                    }
+                    config.AddEnvironmentVariables("CRAWLER_HOST_");
+                })
+                .ConfigureLogging(config =>
+                {
+                    config.AddSerilog();
+                    var configure = new LoggerConfiguration()
+#if DEBUG
+                        .MinimumLevel.Verbose()
+#else
+						.MinimumLevel.Information()
+#endif
+                        .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+                        .Enrich.FromLogContext()
+                        .WriteTo.Console();
+                    Log.Logger = configure.CreateLogger();
+                })
+                .Build();
+
+
+            await host.RunAsync();
         }
     }
 }
